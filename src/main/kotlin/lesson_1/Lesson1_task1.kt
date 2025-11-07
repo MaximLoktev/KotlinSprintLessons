400f4c37a0e496f25639a054d50adc0fc195d3f5
DJNooZ <loktevmaksim@yandex.ru>
2025-11-07 14:20:32 +0300

    KSL-1-1 fix task

--- src/main/kotlin/lesson_1/Lesson1_task1.kt
+++ src/main/kotlin/lesson_1/Lesson1_task1.kt
@@ -1,10 +1,6 @@
 package org.example.lesson_1
 
 fun main() {
-
     val numberOfOrders: Int = 75
     val letterOfThanks: String = "Спасибо за покупку!"
-
-    println(numberOfOrders)
-    println(letterOfThanks)
 }
