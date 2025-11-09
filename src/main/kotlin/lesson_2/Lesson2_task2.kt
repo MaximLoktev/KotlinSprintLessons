05040f6be84ca42a308d30fe8e0a14ffe28f624a
DJNooZ <loktevmaksim@yandex.ru>
2025-11-09 17:16:17 +0300

    KS-2-2 fix task

--- src/main/kotlin/lesson_2/Lesson2_task2.kt
+++ src/main/kotlin/lesson_2/Lesson2_task2.kt
@@ -1,18 +1,18 @@
 package lesson_2
 
 fun main() {
-    val numberOfWorkers: Byte = 50
-    val numberOfInterns: Byte = 30
+    val numberOfWorkers: Int = 50
+    val numberOfInterns: Int = 30
 
-    val workerSalary: Short = 30_000
-    val internsSalary: Short = 20_000
+    val workerSalary: Int = 30_000
+    val internsSalary: Int = 20_000
 
-    val paymentToAllWorkers: Int = workerSalary * numberOfWorkers
-    val paymentToAllInterns: Int = internsSalary * numberOfInterns
+    val paymentToAllWorkers = workerSalary * numberOfWorkers
+    val paymentToAllInterns = internsSalary * numberOfInterns
 
-    val allPayments: Int = paymentToAllWorkers + paymentToAllInterns
+    val allPayments = paymentToAllWorkers + paymentToAllInterns
 
-    val averageSalaryOfWorker: Int = allPayments / numberOfWorkers
+    val averageSalaryOfWorker = allPayments / numberOfWorkers
 
     println(paymentToAllWorkers)
     println(allPayments)
