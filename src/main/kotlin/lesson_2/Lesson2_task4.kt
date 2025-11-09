cf13f5c8d4e312a15112bdcd04b8ec5a1091d10b
DJNooZ <loktevmaksim@yandex.ru>
2025-11-09 17:19:30 +0300

    KS-2-4 fix task

--- src/main/kotlin/lesson_2/Lesson2_task4.kt
+++ src/main/kotlin/lesson_2/Lesson2_task4.kt
@@ -1,13 +1,13 @@
 package lesson_2
 
 fun main() {
-    val buff: Byte = 20
+    val buff: Int = 20
 
-    val ironOre: Byte = 7
-    val crystalOre: Byte = 11
+    val ironOre: Int = 7
+    val crystalOre: Int = 11
 
-    val bonusIronOre: Int = ironOre * buff / 100
-    val bonusCrystalOre: Int = crystalOre * buff / 100
+    val bonusIronOre = ironOre * buff / 100
+    val bonusCrystalOre = crystalOre * buff / 100
 
     println("Кол-во бонусной железной руды: $bonusIronOre")
     println("Кол-во бонусной кристаллической руды: $bonusCrystalOre")
