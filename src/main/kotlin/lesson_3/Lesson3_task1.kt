86fc882ff5d85a63bc68078eec1156ec663b6800
DJNooZ <loktevmaksim@yandex.ru>
2025-11-11 02:21:35 +0300

    KS-3-1 completed the task

---
+++ src/main/kotlin/lesson_3/Lesson3_task1.kt
@@ -0,0 +1,14 @@
+package lesson_3
+
+fun main() {
+    var greeting = ""
+    val userName = "Maxim"
+
+    greeting = "Good afternoon"
+
+    println("$greeting, $userName!")
+
+    greeting = "Good evening"
+
+    println("$greeting, $userName!")
+}
