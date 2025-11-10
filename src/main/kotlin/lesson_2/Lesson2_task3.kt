b2680bcc173abaf4377acba43839782734329fc6
DJNooZ <loktevmaksim@yandex.ru>
2025-11-11 01:38:30 +0300

    KS-2-3 fix

--- src/main/kotlin/lesson_2/Lesson2_task3.kt
+++ src/main/kotlin/lesson_2/Lesson2_task3.kt
@@ -4,14 +4,16 @@ fun main() {
     val startTime: String = "9:39"
     val travelTime: Int = 457
 
+    val timeInterval: Int = 60
+
     val (hour: Int, minutes: Int) = startTime.split(":").map { it.toInt() }
 
-    val startTimeInMinutes = hour * 60 + minutes
+    val startTimeInMinutes = hour * timeInterval + minutes
 
     val arrivalTimeInMinutes = startTimeInMinutes + travelTime
 
-    val arrivalHour = arrivalTimeInMinutes / 60
-    val arrivalMinutes = arrivalTimeInMinutes % 60
+    val arrivalHour = arrivalTimeInMinutes / timeInterval
+    val arrivalMinutes = arrivalTimeInMinutes % timeInterval
 
     println("%02d:%02d".format(arrivalHour, arrivalMinutes))
 }
