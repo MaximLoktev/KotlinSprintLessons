a6acc7a9cecdb3a101bb36222cd0bc0c85ee69c8
DJNooZ <loktevmaksim@yandex.ru>
2025-11-19 22:43:43 +0300

    KS-6-1 completed the task

--- src/main/kotlin/lesson_6/Lesson6_task1.kt
+++ src/main/kotlin/lesson_6/Lesson6_task1.kt
@@ -1,5 +1,24 @@
 package lesson_6
 
 fun main() {
+    println("--- Регистрация ---")
 
+    print("Введите логин: ")
+    val loginReg = readln()
+
+    print("Введите пароль: ")
+    val passwordReg = readln()
+
+    do {
+        println("--- Авторизации ---")
+
+        print("Введите логин: ")
+        val login = readln()
+
+        print("Введите пароль: ")
+        val password = readln()
+
+    } while (loginReg != login || passwordReg != password)
+
+    println("--- Авторизация прошла успешно ---")
 }
