7e1f62258e0c755e92714045c75621950c1947f8
DJNooZ <loktevmaksim@yandex.ru>
2025-11-19 23:09:35 +0300

    KS-6-3 completed the task

--- src/main/kotlin/lesson_6/Lesson6_task3.kt
+++ src/main/kotlin/lesson_6/Lesson6_task3.kt
@@ -1,5 +1,17 @@
 package lesson_6
 
 fun main() {
+    println("--- Таймер ---")
 
+    print("Введите время таймера(в секундах): ")
+    val numberOfSeconds = readln().toInt()
+
+    var counter = numberOfSeconds
+
+    while (counter > 0) {
+        println("Осталось секунд: ${counter--}")
+        Thread.sleep(1000)
+    }
+
+    println("Время вышло")
 }
