1f59571a836baa94ea68848f5a1b17851300ffb2
DJNooZ <loktevmaksim@yandex.ru>
2025-11-22 16:04:18 +0300

    KS-7-3 completed the task

--- src/main/kotlin/lesson_7/Lesson7_task3.kt
+++ src/main/kotlin/lesson_7/Lesson7_task3.kt
@@ -1,5 +1,14 @@
 package lesson_7
 
 fun main() {
+    print("\nВведите число: ")
+    val number = readln().toInt()
 
+    print("\nЧётные числа от 0 до $number: ")
+
+    for (i in 0..number step 2) {
+        print("$i ")
+    }
+
+    println()
 }
