c0c46b933a36302afbf410ee7ce1b427e31a0355
DJNooZ <loktevmaksim@yandex.ru>
2025-11-22 16:45:48 +0300

    KS-7-5 completed the task

--- src/main/kotlin/lesson_7/Lesson7_task5.kt
+++ src/main/kotlin/lesson_7/Lesson7_task5.kt
@@ -1,5 +1,40 @@
 package lesson_7
 
-fun main() {
+private const val MIN_PASSWORD_LENGTH = 6
 
+fun main() {
+    println("\n--- Генератор сложных паролей ---")
+
+    val lowercase = ('a'..'z').toList()
+    val uppercase = ('A'..'Z').toList()
+    val digits = ('0'..'9').toList()
+
+    val allChars = lowercase + uppercase + digits
+
+    // Гарантируем наличие всех типов символов
+    val chars = mutableListOf(
+        lowercase.random(),
+        uppercase.random(),
+        digits.random()
+    )
+
+    var isSuccess = false
+
+    while (!isSuccess) {
+        print("\nВведите длину пароля (минимум 6 символов): ")
+        val passwordLength = readln().toInt()
+
+        if (passwordLength < MIN_PASSWORD_LENGTH) {
+            println("\nОшибка: минимальная длина пароля - 6 символов")
+            continue
+        } else {
+            for (i in 3 until passwordLength) {
+                chars.add(allChars.random())
+            }
+            isSuccess = true
+        }
+    }
+
+    val password = chars.shuffled().joinToString("")
+    println("\nСгенерированный пароль: $password")
 }
