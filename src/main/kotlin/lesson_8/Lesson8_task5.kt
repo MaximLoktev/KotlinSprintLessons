83dffd5a46d08b4d2acd3d011f4e15a7800818cd
DJNooZ <loktevmaksim@yandex.ru>
2025-11-23 01:38:52 +0300

    KS-8-5 completed the task

--- src/main/kotlin/lesson_8/Lesson8_task5.kt
+++ src/main/kotlin/lesson_8/Lesson8_task5.kt
@@ -1,5 +1,17 @@
 package lesson_8
 
 fun main() {
+    print("\nВведите кол-во ингредиентов: ")
+    val count = readln().toInt()
 
+    val ingredients = Array(count) { "" }
+
+    for (i in 0 until count) {
+        print("\nВведите название ингредиента: ")
+        val item = readln().lowercase()
+
+        ingredients[i] = item
+    }
+
+    println("\nСписок ингредиентов: ${ingredients.joinToString()}")
 }
