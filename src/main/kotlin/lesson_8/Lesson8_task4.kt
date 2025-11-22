e1bf9b1a0cfbe7250ff1f2e4a0a92f175bcd02a9
DJNooZ <loktevmaksim@yandex.ru>
2025-11-23 01:06:00 +0300

    KS-8-4 completed the task

--- src/main/kotlin/lesson_8/Lesson8_task4.kt
+++ src/main/kotlin/lesson_8/Lesson8_task4.kt
@@ -1,5 +1,24 @@
 package lesson_8
 
 fun main() {
+    val ingredients = arrayOf("говядина", "вода", "лук", "морковь", "свёкла")
 
+    println("\nТекущий список ингредиентов: ${ingredients.contentToString()}")
+
+    print("\nКакой ингредиент нужно заменить: ")
+    val searchQuery = readln().lowercase()
+
+    if (ingredients.contains(searchQuery)) {
+        println("\nИнгредиент $searchQuery в рецепте есть")
+
+        print("\nКакой ингредиент нужно добавить: ")
+        val newItem = readln().lowercase()
+
+        val index = ingredients.indexOf(searchQuery)
+        ingredients[index] = newItem
+
+        println("\nГотово! Вы сохранили следующий список: ${ingredients.contentToString()}")
+    } else {
+        println("\nТакого ингредиента в рецепте нет")
+    }
 }
