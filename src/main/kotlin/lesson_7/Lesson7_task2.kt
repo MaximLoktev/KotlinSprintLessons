9f726b90064819b9f5e4ec430818ce6c60d3337b
DJNooZ <loktevmaksim@yandex.ru>
2025-11-22 15:57:31 +0300

    KS-7-2 completed the task

--- src/main/kotlin/lesson_7/Lesson7_task2.kt
+++ src/main/kotlin/lesson_7/Lesson7_task2.kt
@@ -1,5 +1,23 @@
 package lesson_7
 
 fun main() {
+    println("\n--- Система авторизации по SMS ---")
 
+    var isAuthorized = false
+
+    while (!isAuthorized) {
+        val smsCode = (1000..9999).random().toString()
+        println("Ваш код авторизации: $smsCode")
+
+        print("\nВведите код из SMS: ")
+        val userInput = readln()
+
+        if (userInput == smsCode) {
+            println("\nАвторизация успешна!")
+            println("Добро пожаловать в систему!")
+            isAuthorized = true
+        } else {
+            println("\nНеверный код! Отправляем новый...")
+        }
+    }
 }
