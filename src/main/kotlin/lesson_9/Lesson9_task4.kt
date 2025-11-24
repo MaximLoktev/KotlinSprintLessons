1681335af71674e6a388af97fe891cdc279d4b0a
DJNooZ <loktevmaksim@yandex.ru>
2025-11-24 20:38:50 +0300

    KS-9-4 completed the task

--- src/main/kotlin/lesson_9/Lesson9_task4.kt
+++ src/main/kotlin/lesson_9/Lesson9_task4.kt
@@ -1,5 +1,19 @@
 package lesson_9
 
-fun main() {
+private const val NUMBER_OF_INGREDIENTS = 5
 
+fun main() {
+    print("Введите $NUMBER_OF_INGREDIENTS ингредиентов через запятую с пробелом (, ): ")
+    val input = readln().lowercase()
+
+    val ingredients = input.split(", ")
+
+    if (ingredients.size != NUMBER_OF_INGREDIENTS) {
+        println("Ошибка: нужно ввести ровно $NUMBER_OF_INGREDIENTS ингредиентов через ', '")
+        return
+    }
+
+    val sortedIngredients = ingredients.sorted()
+
+    println("Отсортированный список ингредиентов: $sortedIngredients")
 }
