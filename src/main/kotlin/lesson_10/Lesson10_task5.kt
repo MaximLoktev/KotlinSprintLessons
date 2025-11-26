bca3d66b27ec436b4215b03ec530281597c13648
DJNooZ <loktevmaksim@yandex.ru>
2025-11-27 01:07:46 +0300

    KS-10-5 completed the task

--- src/main/kotlin/lesson_10/Lesson10_task5.kt
+++ src/main/kotlin/lesson_10/Lesson10_task5.kt
@@ -1,5 +1,27 @@
 package lesson_10
 
-fun main() {
+const val CORRECT_LOGIN = "admin"
+const val CORRECT_PASSWORD = "qwerty"
 
+fun main() {
+    val login = "admin"
+    val password = "qwerty"
+
+    val authToken = authorize(login, password)
+
+    println(
+        if (authToken != null) getShoppingCart(authToken)
+            else "Неудачная авторизация: неверный логин или пароль"
+    )
 }
+
+fun authorize(login: String, password: String): String? =
+    if (login == CORRECT_LOGIN && password == CORRECT_PASSWORD) generateToken() else null
+
+fun generateToken(): String {
+    val characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+    return String(CharArray(32) { characters.random() })
+}
+
+fun getShoppingCart(token: String): List<String> =
+    listOf("яблоко", "банан", "апельсин", "груша", "арбуз")
