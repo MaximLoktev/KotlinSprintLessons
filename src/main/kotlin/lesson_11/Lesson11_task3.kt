cfb6acbdbaca472dad7743ae2d4c1c7316de2477
DJNooZ <loktevmaksim@yandex.ru>
2025-11-27 20:47:29 +0300

    KS-11-3 completed the task

--- src/main/kotlin/lesson_11/Lesson11_task3.kt
+++ src/main/kotlin/lesson_11/Lesson11_task3.kt
@@ -1,5 +1,26 @@
 package lesson_11
 
 fun main() {
+    val gameRoom = VoiceRoom(
+        id = 1,
+        coverImage = "game_cover.jpg",
+        name = "Игровая комната"
+    )
 
+    val users = listOf(
+        User(id = 1, name = "Max", avatar = "111.jpg", status = UserStatus.SPEAKING),
+        User(id = 2, name = "Djon", avatar = "222.jpg", status = UserStatus.MUTED),
+        User(id = 3, name = "Alex", avatar = "333.jpg", status = UserStatus.MICROPHONE_OFF)
+    )
+
+    users.forEach {
+        gameRoom.addParticipant(it)
+    }
+
+    gameRoom.updateUserStatus(
+        username = users[0].name,
+        newStatus = UserStatus.MUTED
+    )
+
+    gameRoom.printRoomInfo()
 }
