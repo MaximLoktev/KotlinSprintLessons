1bfeef9140fcf2f83ec1d6d47941dc0644526634
DJNooZ <loktevmaksim@yandex.ru>
2025-11-29 01:59:14 +0300

    KS-11-3 fix files

--- src/main/kotlin/lesson_11/Lesson11_task3.kt
+++ src/main/kotlin/lesson_11/Lesson11_task3.kt
@@ -24,3 +24,74 @@ fun main() {
 
     gameRoom.printRoomInfo()
 }
+
+enum class UserStatus(val string: String) {
+    SPEAKING("разговаривает"),
+    MICROPHONE_OFF("микрофон выключен"),
+    MUTED("пользователь заглушен"),
+}
+
+class User(
+    val id: Int,
+    val name: String,
+    val avatar: String,
+    var status: UserStatus,
+) {
+    fun printUserInfo() {
+        println("""
+            Идентификатор: $id
+            Имя: $name
+            Аватар: $avatar
+            Статус: ${status.string}
+        """.trimIndent())
+    }
+
+    fun getStatusText(): String = status.string
+
+    fun setNewStatus(value: UserStatus) {
+        status = value
+        println("Статус изменён!")
+    }
+}
+
+class VoiceRoom(
+    val id: Int,
+    val coverImage: String,
+    val name: String
+) {
+    private val participants = mutableListOf<User>()
+
+    fun addParticipant(user: User) {
+        participants.add(user)
+        println("Пользователь '${user.name}' присоединился к комнате '$name'")
+    }
+
+    fun updateUserStatus(username: String, newStatus: UserStatus) {
+        val user = participants.find { it.name == username }
+
+        if (user != null) {
+            user.status = newStatus
+
+            println("\nПользователь '${user.name}' сменил статус на '${newStatus.string}'")
+        } else {
+            println("Пользователь '$username' не найден в комнате")
+        }
+    }
+
+    fun printRoomInfo() {
+        println("\n- Информация о комнате -")
+        println("""
+            Идентификатор: $id
+            Название: $name
+            Обложка: $coverImage
+            Участники: ${participants.size}
+        """.trimIndent())
+
+        println("\n- Информация об участниках -")
+
+        participants.forEachIndexed { index, user ->
+            println("\n- Участник ${index + 1} -")
+            user.printUserInfo()
+        }
+    }
+}
