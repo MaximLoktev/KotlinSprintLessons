2e939d91faeb953c078acd884feee64c6e742f8e
DJNooZ <loktevmaksim@yandex.ru>
2025-11-29 17:44:44 +0300

    KS-12-3 fix task

--- src/main/kotlin/lesson_12/Lesson12_task3.kt
+++ src/main/kotlin/lesson_12/Lesson12_task3.kt
@@ -2,22 +2,22 @@ package lesson_12
 
 fun main() {
     val firstDay = WeatherData3(
-        dayTempKelvin = 293.15,
-        nightTempKelvin = 287.15,
+        dayTempKelvin = 293,
+        nightTempKelvin = 287,
         _hasPrecipitation = false,
     )
     firstDay.printWeatherData("Среда")
 }
 
+private const val ABSOLUTE_ZERO_CELSIUS = -273
+
 class WeatherData3(
-    dayTempKelvin: Double,
-    nightTempKelvin: Double,
+    dayTempKelvin: Int,
+    nightTempKelvin: Int,
     _hasPrecipitation: Boolean,
 ) {
-    private val ABSOLUTE_ZERO_CELSIUS = -273.15
-
-    val dayTemp: Int = (dayTempKelvin + ABSOLUTE_ZERO_CELSIUS).toInt()
-    val nightTemp: Int = (nightTempKelvin + ABSOLUTE_ZERO_CELSIUS).toInt()
+    val dayTemp = (dayTempKelvin + ABSOLUTE_ZERO_CELSIUS)
+    val nightTemp = (nightTempKelvin + ABSOLUTE_ZERO_CELSIUS)
     val hasPrecipitation = _hasPrecipitation
 
     fun printWeatherData(dayOfWeek: String) {
