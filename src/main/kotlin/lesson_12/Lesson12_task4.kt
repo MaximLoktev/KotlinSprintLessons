0c1b4f6f59f961e95b86aacfd205af5317f01a51
DJNooZ <loktevmaksim@yandex.ru>
2025-11-29 14:24:38 +0300

    KS-12-4 completed the task

--- src/main/kotlin/lesson_12/Lesson12_task4.kt
+++ src/main/kotlin/lesson_12/Lesson12_task4.kt
@@ -1,2 +1,31 @@
 package lesson_12
 
+fun main() {
+    val firstDay = WeatherData4(
+        dayTempKelvin = 293.15,
+        nightTempKelvin = 287.15,
+        _hasPrecipitation = false,
+    )
+}
+
+class WeatherData4(
+    dayTempKelvin: Double,
+    nightTempKelvin: Double,
+    _hasPrecipitation: Boolean,
+) {
+    private val ABSOLUTE_ZERO_CELSIUS = -273.15
+
+    val dayTemp: Int = (dayTempKelvin + ABSOLUTE_ZERO_CELSIUS).toInt()
+    val nightTemp: Int = (nightTempKelvin + ABSOLUTE_ZERO_CELSIUS).toInt()
+    val hasPrecipitation = _hasPrecipitation
+
+    init {
+        printWeatherData(dayOfWeek = "Пятница")
+    }
+
+    fun printWeatherData(dayOfWeek: String) {
+        val precipitation = if (hasPrecipitation) "Ожидаются осадки" else "Без осадков"
+
+        println("$dayOfWeek: Днём $dayTemp°C, Ночью $nightTemp°C, $precipitation")
+    }
+}
