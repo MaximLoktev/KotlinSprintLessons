41b18f44670571ab4e13c44e7fccd70876dbfe4a
DJNooZ <loktevmaksim@yandex.ru>
2025-11-30 23:17:17 +0300

    KS-14-1 completed the task

--- src/main/kotlin/lesson_14/Lesson14_task1.kt
+++ src/main/kotlin/lesson_14/Lesson14_task1.kt
@@ -1,2 +1,26 @@
 package lesson_14
 
+fun main() {
+    val liner = Liner()
+    val icebreaker = Icebreaker()
+    val cargoShip = CargoShip()
+}
+
+open class Liner(
+    val maxSpeed: Int = 50,
+    val passengerCapacity: Int = 100,
+    val cargoCapacity: Int = 50,
+)
+
+class Icebreaker(
+    val canBreakIce: Boolean = true
+) : Liner(
+    maxSpeed = 30,
+    passengerCapacity = 50,
+)
+
+class CargoShip : Liner(
+    maxSpeed = 10,
+    passengerCapacity = 50,
+    cargoCapacity = 1000,
+)
