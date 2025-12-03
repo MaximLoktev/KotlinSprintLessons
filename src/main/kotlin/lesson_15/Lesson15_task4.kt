9c9028fd159204a2e8dd1c5ad3c73e46807d4daf
DJNooZ <loktevmaksim@yandex.ru>
2025-12-03 23:41:51 +0300

    KS-15-4 completed the task

--- src/main/kotlin/lesson_15/Lesson15_task4.kt
+++ src/main/kotlin/lesson_15/Lesson15_task4.kt
@@ -1,2 +1,18 @@
 package lesson_15
 
+abstract class Product(
+    val name: String,
+    val count: Int,
+)
+
+interface Searchable {
+    fun search(query: String)
+}
+
+class Tools(name: String, count: Int) : Product(name, count), Searchable {
+
+    override fun search(query: String) =
+        println("Выполняется поиск")
+}
+
+class Accessories(name: String, count: Int) : Product(name, count)
