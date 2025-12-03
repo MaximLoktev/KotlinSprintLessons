e34740a84f5e41ef57ba021a9841ff3c507cc84d
DJNooZ <loktevmaksim@yandex.ru>
2025-12-03 23:03:48 +0300

    KS-15-2 completed the task

--- src/main/kotlin/lesson_15/Lesson15_task2.kt
+++ src/main/kotlin/lesson_15/Lesson15_task2.kt
@@ -1,2 +1,56 @@
 package lesson_15
 
+fun main() {
+    val weatherServer = WeatherServer()
+    weatherServer.sendData(
+        data = Temperature(stationId = "1", temperature = 35.0)
+    )
+    weatherServer.sendData(
+        data = PrecipitationAmount(stationId = "2", amount = 15.8)
+    )
+}
+
+abstract class WeatherStationStats(val stationId: String) {
+    abstract fun getData(): String
+    abstract fun getUnit(): String
+
+    fun getFormattedData(): String {
+        return "${getData()} ${getUnit()}"
+    }
+}
+
+class Temperature(
+    stationId: String,
+    val temperature: Double,
+) : WeatherStationStats(stationId) {
+
+    override fun getData(): String = "%.1f".format(temperature)
+
+    override fun getUnit(): String = "°C"
+}
+
+class PrecipitationAmount(
+    stationId: String,
+    val amount: Double,
+    val precipitationType: String = "дождь",
+) : WeatherStationStats(stationId) {
+
+    override fun getData(): String = "%.1f".format(amount)
+
+    override fun getUnit(): String = "мм"
+}
+
+class WeatherServer {
+
+    fun sendData(data: WeatherStationStats) {
+        println("\nПолучены данные со станции №${data.stationId}")
+
+        val dataType = when (data) {
+            is Temperature -> "Температура"
+            is PrecipitationAmount -> "Осадки"
+            else -> ""
+        }
+
+        println("$dataType: ${data.getFormattedData()}")
+    }
+}
