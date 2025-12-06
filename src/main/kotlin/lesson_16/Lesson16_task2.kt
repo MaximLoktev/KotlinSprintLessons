3f87e59c3dd0a74071974e627fe9c4237865870c
DJNooZ <loktevmaksim@yandex.ru>
2025-12-06 13:54:26 +0300

    KS-16-2 completed the task

--- src/main/kotlin/lesson_16/Lesson16_task2.kt
+++ src/main/kotlin/lesson_16/Lesson16_task2.kt
@@ -1,2 +1,16 @@
 package lesson_16
 
+fun main() {
+    val circle = Circle(13.0)
+    println("Длина окружности: ${circle.getCircumference()}")
+    println("Площадь круга: ${circle.getArea()}")
+}
+
+private const val PI = 3.14
+
+class Circle(private val radius: Double) {
+
+    fun getCircumference(): Double = 2 * PI * radius
+
+    fun getArea(): Double = PI * radius * radius
+}
