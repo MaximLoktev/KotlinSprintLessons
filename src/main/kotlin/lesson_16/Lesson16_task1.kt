a744dac526dc81b82d6bc7de2d90d6b59f87460c
DJNooZ <loktevmaksim@yandex.ru>
2025-12-06 13:32:29 +0300

    KS-16-1 completed the task

--- src/main/kotlin/lesson_16/Lesson16_task1.kt
+++ src/main/kotlin/lesson_16/Lesson16_task1.kt
@@ -1,2 +1,14 @@
 package lesson_16
 
+fun main() {
+    val dice = Dice()
+    dice.showValue()
+}
+
+class Dice {
+    private val value = (1..6).random()
+
+    fun showValue() {
+        println("На кубике выпало: $value")
+    }
+}
