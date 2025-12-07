92c8ed8d80ac96ad91f10fff76180597317c1eb7
DJNooZ <loktevmaksim@yandex.ru>
2025-12-07 21:27:50 +0300

    KS-18-2 completed the task

--- src/main/kotlin/lesson_18/Lesson18_task2.kt
+++ src/main/kotlin/lesson_18/Lesson18_task2.kt
@@ -1,2 +1,21 @@
 package lesson_18
 
+fun main() {
+    val diceList: List<Dice> = listOf(D4(), D6(), D8())
+
+    println("Бросаем кости:")
+    for (dice in diceList) {
+        dice.roll()
+    }
+}
+
+open class Dice(val sides: Int) {
+    open fun roll() {
+        val result = (1..sides).random()
+        println("Кость D$sides выпало: $result")
+    }
+}
+
+class D4 : Dice(sides = 4)
+class D6 : Dice(sides = 6)
+class D8 : Dice(sides = 8)
