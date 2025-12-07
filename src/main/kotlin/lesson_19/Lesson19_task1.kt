7ba580d5ff860aa173a229b52e86a41db8868111
DJNooZ <loktevmaksim@yandex.ru>
2025-12-07 23:56:54 +0300

    KS-19-1 completed the task

--- src/main/kotlin/lesson_19/Lesson19_task1.kt
+++ src/main/kotlin/lesson_19/Lesson19_task1.kt
@@ -1,2 +1,16 @@
 package lesson_19
 
+fun main() {
+    println("Вы можете добавить в аквариум следующих рыб:")
+
+    FishType.entries.forEach {
+        println("- ${it.russianName}")
+    }
+}
+
+enum class FishType(val russianName: String) {
+    GUPPY("гуппи"),
+    ANGELFISH("скалярия"),
+    GOLDFISH("золотая рыбка"),
+    SIAMESE_FIGHTING_FISH("петушок")
+}
