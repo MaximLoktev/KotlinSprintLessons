ff873ac9d6d6c0a6b5b1457e8ff7c4cbada51b1b
DJNooZ <loktevmaksim@yandex.ru>
2025-12-08 00:03:10 +0300

    KS-19-2 completed the task

--- src/main/kotlin/lesson_19/Lesson19_task2.kt
+++ src/main/kotlin/lesson_19/Lesson19_task2.kt
@@ -1,2 +1,39 @@
 package lesson_19
 
+fun main() {
+    val product1 = Product("Футболка", 1, Category.CLOTHING)
+    val product2 = Product("Ручка шариковая", 2, Category.STATIONERY)
+    val product3 = Product("Настольная лампа", 3, Category.OTHER)
+
+    println("\nИнформация о товарах:")
+    product1.printInfo()
+    product2.printInfo()
+    product3.printInfo()
+}
+
+enum class Category {
+    CLOTHING {
+        override fun getDisplayName() = "Одежда"
+    },
+    STATIONERY {
+        override fun getDisplayName() = "Канцелярские товары"
+    },
+    OTHER {
+        override fun getDisplayName() = "Разное"
+    };
+
+    abstract fun getDisplayName(): String
+}
+
+class Product(
+    val name: String,
+    val id: Int,
+    val category: Category
+) {
+    fun printInfo() {
+        println("Товар: $name")
+        println("ID: $id")
+        println("Категория: ${category.getDisplayName()}")
+        println("---")
+    }
+}
