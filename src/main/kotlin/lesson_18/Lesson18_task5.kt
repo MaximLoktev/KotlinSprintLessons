a322e67586847a85656d7d100460371451f8b54d
DJNooZ <loktevmaksim@yandex.ru>
2025-12-07 23:33:41 +0300

    KS-18-5 fix task

--- src/main/kotlin/lesson_18/Lesson18_task5.kt
+++ src/main/kotlin/lesson_18/Lesson18_task5.kt
@@ -3,19 +3,31 @@ package lesson_18
 fun main() {
     val screen = Screen()
 
-    println(screen.draw("круг", 10, 20))
-    println(screen.draw("квадрат", 5, 15))
-    println(screen.draw("точка", 0, 0))
+    println(screen.drawCircle(10, 20))
+    println(screen.drawSquare(5, 15))
+    println(screen.drawPoint(0, 0))
 
-    println(screen.draw("круг", 10.5f, 20.3f))
-    println(screen.draw("квадрат", 5.7f, 15.2f))
-    println(screen.draw("точка", 0.0f, 0.0f))
+    println(screen.drawCircle(10.5f, 20.3f))
+    println(screen.drawSquare(5.7f, 15.2f))
+    println(screen.drawPoint(0.0f, 0.0f))
 }
 
 class Screen {
-    fun draw(shape: String, x: Int, y: Int): String =
-        "Нарисован(а) $shape в координатах ($x, $y)"
+    fun drawCircle(x: Int, y: Int): String =
+        "Нарисован круг в координатах ($x, $y)"
 
-    fun draw(shape: String, x: Float, y: Float): String =
-        "Нарисована(а) $shape в координатах (${"%.1f".format(x)}, ${"%.1f".format(y)})"
+    fun drawCircle(x: Float, y: Float): String =
+        "Нарисован круг в координатах (${"%.1f".format(x)}, ${"%.1f".format(y)})"
+
+    fun drawSquare(x: Int, y: Int): String =
+        "Нарисован квадрат в координатах ($x, $y)"
+
+    fun drawSquare(x: Float, y: Float): String =
+        "Нарисован квадрат в координатах (${"%.1f".format(x)}, ${"%.1f".format(y)})"
+
+    fun drawPoint(x: Int, y: Int): String =
+        "Нарисована точка в координатах ($x, $y)"
+
+    fun drawPoint(x: Float, y: Float): String =
+        "Нарисована точка в координатах (${"%.1f".format(x)}, ${"%.1f".format(y)})"
 }
