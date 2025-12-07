52899a1ab301cad1bc1f8e4792b26eb4fcdd8178
DJNooZ <loktevmaksim@yandex.ru>
2025-12-07 21:51:10 +0300

    KS-18-4 completed the task

--- src/main/kotlin/lesson_18/Lesson18_task4.kt
+++ src/main/kotlin/lesson_18/Lesson18_task4.kt
@@ -1,2 +1,30 @@
 package lesson_18
 
+fun main() {
+    val boxes: List<Box> = listOf(
+        RectangularBox(10.0, 5.0, 3.0),
+        CubeBox(4.0),
+    )
+
+    boxes.forEach {
+        println("Площадь: ${it.surfaceArea()}")
+    }
+}
+
+abstract class Box {
+    abstract fun surfaceArea(): Double
+}
+
+class RectangularBox(
+    val length: Double,
+    val width: Double,
+    val height: Double,
+) : Box() {
+    override fun surfaceArea(): Double {
+        return 2 * (length * width + length * height + width * height)
+    }
+}
+
+class CubeBox(val side: Double) : Box() {
+    override fun surfaceArea(): Double = 6 * side * side
+}
