c3f877b0568cedfbbd2ac16c88e18ba97e84b786
DJNooZ <loktevmaksim@yandex.ru>
2025-12-08 00:12:33 +0300

    KS-19-3 completed the task

--- src/main/kotlin/lesson_19/Lesson19_task3.kt
+++ src/main/kotlin/lesson_19/Lesson19_task3.kt
@@ -1,2 +1,22 @@
 package lesson_19
 
+fun main() {
+    val ship = Spaceship()
+    ship.takeOff()
+    ship.land()
+    ship.shootAsteroid()
+}
+
+class Spaceship {
+    fun takeOff() {
+        println("Корабль взлетает")
+    }
+
+    fun land() {
+        // TODO: нужна информация о дополнительной логике
+    }
+
+    fun shootAsteroid() {
+        TODO("Метод отстрела астероидов еще не реализован")
+    }
+}
