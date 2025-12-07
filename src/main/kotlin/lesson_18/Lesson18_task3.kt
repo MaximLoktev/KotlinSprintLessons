7a22f811f7eda8b983f47385566b201d0d3703ab
DJNooZ <loktevmaksim@yandex.ru>
2025-12-07 21:36:06 +0300

    KS-18-3 completed the task

--- src/main/kotlin/lesson_18/Lesson18_task3.kt
+++ src/main/kotlin/lesson_18/Lesson18_task3.kt
@@ -1,2 +1,37 @@
 package lesson_18
 
+fun main() {
+    val animals: List<Animal> = listOf(
+        Fox("Рыжик"),
+        Dog("Бобик"),
+        Cat("Мурка")
+    )
+
+    animals.forEach { it.eat() }
+}
+
+abstract class Animal(val name: String) {
+    abstract fun eat()
+
+    fun sleep() {
+        println("$name -> спит")
+    }
+}
+
+class Fox(name: String) : Animal(name) {
+    override fun eat() {
+        println("$name -> ест ягоды")
+    }
+}
+
+class Dog(name: String) : Animal(name) {
+    override fun eat() {
+        println("$name -> ест кости")
+    }
+}
+
+class Cat(name: String) : Animal(name) {
+    override fun eat() {
+        println("$name -> ест рыбу")
+    }
+}
