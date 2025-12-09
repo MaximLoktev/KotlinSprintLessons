fa1a5ddfc34989cce9653a5e6dc040a37113bc14
DJNooZ <loktevmaksim@yandex.ru>
2025-12-09 22:24:25 +0300

    KS-20-4 completed the task

--- src/main/kotlin/lesson_20/Lesson20_task4.kt
+++ src/main/kotlin/lesson_20/Lesson20_task4.kt
@@ -1,2 +1,16 @@
 package lesson_20
 
+fun main() {
+    val items = listOf("Кнопка", "Ссылка", "Поле", "Чекбокс", "Слайдер")
+
+    val clickActions = items.map { item ->
+        { println("Нажат элемент $item") }
+    }
+
+    println("Нажимаем четные элементы:")
+    clickActions.forEachIndexed { index, action ->
+        if ((index + 1) % 2 == 0) {
+            action()
+        }
+    }
+}
