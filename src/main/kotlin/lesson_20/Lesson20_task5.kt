4a0fbe1796c6b3d0a49ca1bcc2f5aaf43f1eb772
DJNooZ <loktevmaksim@yandex.ru>
2025-12-09 22:32:19 +0300

    KS-20-5 completed the task

--- src/main/kotlin/lesson_20/Lesson20_task5.kt
+++ src/main/kotlin/lesson_20/Lesson20_task5.kt
@@ -1,2 +1,34 @@
 package lesson_20
 
+fun main() {
+    val robot = Robot()
+
+    println("Обычная речь:")
+    robot.say()
+
+    robot.setModifier { phrase ->
+        phrase.reversed()
+    }
+
+    println("\nИнвертированная речь:")
+    robot.say()
+}
+
+class Robot {
+
+    private val phrases = listOf(
+        "Привет, я робот",
+        "Как ваши дела?",
+        "Я умею говорить",
+        "Программирование это весело",
+        "Давайте работать вместе"
+    )
+
+    private var modifier: (String) -> String = { it }
+
+    fun setModifier(mod: (String) -> String) { modifier = mod }
+
+    fun say() {
+        println(modifier(phrases.random()))
+    }
+}
