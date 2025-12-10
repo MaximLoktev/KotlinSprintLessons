eef43eae960d188b0939ef989fb9708b18ca8d1e
DJNooZ <loktevmaksim@yandex.ru>
2025-12-10 19:34:13 +0300

    KS-21-4 completed the task

--- src/main/kotlin/lesson_21/Lesson21_task4.kt
+++ src/main/kotlin/lesson_21/Lesson21_task4.kt
@@ -1,2 +1,18 @@
 package lesson_21
 
+import java.io.File
+
+fun main() {
+    val file = File("words.txt")
+    file.writeText("world")
+
+    println("Было: ${file.readText()}")
+
+    file.prependWord("Hello")
+
+    println("Стало: ${file.readText()}")
+}
+
+fun File.prependWord(word: String) {
+    this.writeText("${word.lowercase()} ${this.readText()}")
+}
