e987f03cbdcf79c3b3716e12941c969b698eb856
DJNooZ <loktevmaksim@yandex.ru>
2025-12-10 19:15:00 +0300

    S-21-1 completed the task

--- src/main/kotlin/lesson_21/Lesson21_task1.kt
+++ src/main/kotlin/lesson_21/Lesson21_task1.kt
@@ -1,2 +1,9 @@
 package lesson_21
 
+fun main() {
+    println("hello".vowelCount())
+    println("Good morning".vowelCount())
+}
+
+fun String.vowelCount(): Int =
+    this.lowercase().count { it in "aeiouy" }
