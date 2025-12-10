3ebef9dba488a35a0d4036cde2aa0b395103a103
DJNooZ <loktevmaksim@yandex.ru>
2025-12-10 19:40:51 +0300

    KS-21-5 completed the task

--- src/main/kotlin/lesson_21/Lesson21_task5.kt
+++ src/main/kotlin/lesson_21/Lesson21_task5.kt
@@ -1,2 +1,13 @@
 package lesson_21
 
+fun main() {
+    val skills = mapOf(
+        "Сила" to 33,
+        "Ловкость" to 75,
+        "Интеллект" to 45,
+        "Крит" to 75,
+    )
+    println(skills.maxCategory())
+}
+
+fun Map<String, Int>.maxCategory(): String = this.maxBy { it.value }.key
