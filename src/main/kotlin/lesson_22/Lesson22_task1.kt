3e03b04f390cbab7c5755a0ef1ffb83cf9dd2130
DJNooZ <loktevmaksim@yandex.ru>
2025-12-11 19:20:36 +0300

    KS-22-1 completed the task

--- src/main/kotlin/lesson_22/Lesson22_task1.kt
+++ src/main/kotlin/lesson_22/Lesson22_task1.kt
@@ -1,2 +1,19 @@
 package lesson_22
 
+fun main() {
+    val regularBook1 = RegularBook("Дюна", "Фрэнк Герберт")
+    val regularBook2 = RegularBook("Дюна", "Фрэнк Герберт")
+
+    val dataBook1 = DataBook("Дюна", "Фрэнк Герберт")
+    val dataBook2 = DataBook("Дюна", "Фрэнк Герберт")
+
+    // В случае class оператор == сравнивает ССЫЛКИ на объекты (адреса в памяти)
+    println(regularBook1 == regularBook2)
+
+    // В случае data class оператор == сравнивает СОДЕРЖИМОЕ объектов(значения свойств)
+    println(dataBook1 == dataBook2)
+}
+
+class RegularBook(val title: String, val author: String)
+
+data class DataBook(val title: String, val author: String)
