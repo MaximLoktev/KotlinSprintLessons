62c58cbe871cccee8792f8bbed8c6be9d653c679
DJNooZ <loktevmaksim@yandex.ru>
2025-12-11 19:50:23 +0300

    KS-22-3 completed the task

--- src/main/kotlin/lesson_22/Lesson22_task3.kt
+++ src/main/kotlin/lesson_22/Lesson22_task3.kt
@@ -1,2 +1,15 @@
 package lesson_22
 
+fun main() {
+    val (title, author, copies) = Book("Дюна", "Фрэнк Герберт", 33)
+
+    println("Название: $title")
+    println("Автор: $author")
+    println("Кол-во копий: $copies")
+}
+
+data class Book(
+    val title: String,
+    val author: String,
+    val copies: Int,
+)
