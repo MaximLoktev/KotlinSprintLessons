d12a226719b07e1e74dd44eb8d10b1dd7e0952be
DJNooZ <loktevmaksim@yandex.ru>
2025-12-11 20:11:04 +0300

    KS-22-4 completed the task

--- src/main/kotlin/lesson_22/Lesson22_task4.kt
+++ src/main/kotlin/lesson_22/Lesson22_task4.kt
@@ -1,2 +1,28 @@
 package lesson_22
 
+fun main() {
+    val viewModel = MainScreenViewModel()
+    viewModel.loadData()
+}
+
+data class MainScreenState(
+    val data: String = "",
+    val isLoading: Boolean = false
+)
+
+class MainScreenViewModel {
+    private var state = MainScreenState()
+
+    fun loadData() {
+        println("Состояние 1: $state")
+
+        state = state.copy(isLoading = true)
+        println("Состояние 2: $state")
+
+        state = state.copy(
+            data = "Данные успешно загружены",
+            isLoading = false
+        )
+        println("Состояние 3: $state")
+    }
+}
